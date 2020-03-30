593b743470349be4889b410eeba3b032071c14f9
jackjsj <jackjsj@foxmail.com>
2020-03-30 08:41:52 +0800

    update

--- 手写Object.create/createObj.js
+++ 手写Object.create/createObj.js
@@ -10,6 +10,9 @@ function createObj(obj) {
   function fn() {}
   fn.prototype = obj;
   return new fn();
+  // const _obj = {};
+  // _obj.__proto__ = obj; // 直接修改__proto__性能不好
+  // return _obj;
 }
 
 const obj = createObj(Person.prototype);
--- 手写继生组合式继承/extend.js
+++ 手写继生组合式继承/extend.js
@@ -9,6 +9,8 @@ function Child(name, age) {
   Parent.call(this, name);
   this.age = age;
 }
+// Object.setPrototypeOf(Child.prototype, Parent.prototype); // 官方说性能不好
+// Child.prototype.__proto__ = Parent.prototype; // __proto__不鼓励使用，直接修改__proto__性能不好
 
 Child.prototype = Object.create(Parent.prototype);
 Child.prototype.constructor = Child;
